ee3bd06fe1b35340bf61ea20a8bffb9172099f8a
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-10-01 11:33:30 +0600

    [ADD] categories

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://cdn.pixabay.com/photo/2019/08/01/04/31/girl-4376600_960_720.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2019/08/01/04/32/girl-4376611_960_720.jpg",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2019/08/01/04/31/girl-4376602_960_720.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5faf4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://cdn.pixabay.com/photo/2017/02/28/08/29/girl-2105150_960_720.jpg",
+    title: "SUMMER SALE",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2017/02/28/08/37/girl-2105180_960_720.jpg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2017/02/28/08/29/girl-2105149_960_720.jpg",
+    title: "LIGHT JACKETS",
+  },
+];
